2e4f9b49e8371d6c25e87b898d470ce8f4fda4b8
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-10-31 08:54:10 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,59 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int i, negativi, p;
+    float numero, min, max, somma, media;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    //Inizializazione variabili
+    somma=0;
+    i=1;
+    negativi=0;
+    p=0;,
+
+    //Inserimento numeri
+    do
+    {
+        cout>>"Inserire un numero"<<endl;
+        cin>>numero;
+
+        if (numero>=0)
+        {
+            somma=somma+numero;
+            p=p+1
+        }
+
+        if (numero<0)
+        {
+            negativi=negativi+1;
+        }
+
+        if (i==1) 
+        {
+            max=numero;
+            min=numero;
+        }
+        else
+        {
+            if (numero>max)
+            {
+                max=numero;
+            }
+
+            if (numero<min)
+            {
+                min=numero;
+            }
+        }
+
+        i=i+1;
+    }
+    while (numero!=0)
+
+    media=somma/p;
+
+    cout<<"Massimo: "<<max<<ednl;
+    cout<<"Minimo: "<<min<<endl;
+
+    cout<<"Media numeri positivi: "<<media<<endl;
+    cout<<"Numeri negativi: "<<negativi<<endl;
+}
